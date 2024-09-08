d23e742332d8e4f06b122bfb4b3f9c5d81d0fb2e
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-09-08 21:34:00 +0200

    Initial commit

---
+++ backend/src/main/java/de/ricardo/DiaryApp.java
@@ -0,0 +1,14 @@
+package de.ricardo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiaryApp.class, args);
+    }
+
+}
